e6c9a18dc0f0e0b7e32263c87e7df7103182eaf8
Berni Kleiner <berni.kleiner@gmail.com>
2019-03-26 23:28:04 -0600

    add parse from file method

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,36 @@
+import java.io.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Reading data from file");
+        String filename = "input.txt";
+        BinSearchTree studentData = readFile(filename);
+    }
+
+    public static BinSearchTree readFile(String filename){
+        File file;
+        BufferedReader br;
+        BinSearchTree BST = new BinSearchTree();
+        try{
+            file = new File(filename);
+            br = new BufferedReader(new FileReader(file));
+                try {
+                    String line;
+                    while ((line = br.readLine()) != null){
+                        String[] splitLine = line.split("\\s+");
+                        BST.insert(splitLine[1], splitLine[2], splitLine[3], splitLine[4]);
+                    }
+
+                } catch (IOException e) {
+                    System.out.println("IO exception");
+                    e.printStackTrace();
+                }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found exception");
+            e.printStackTrace();
+        }
+        return BST;
     }
 }
